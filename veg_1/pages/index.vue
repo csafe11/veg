e48c1a8ed70685362d9c09fc24a76be993c20773
csafe11 <biztonsagos01@gmail.com>
2025-03-17 22:58:42 +0100

    negyedik

--- veg_1/pages/index.vue
+++ veg_1/pages/index.vue
@@ -1,14 +1,22 @@
 
 
 <template>
- <div class="columns-3">
-<NuxtLink to="/receptek">Receptek</NuxtLink>
-<NuxtLink to="/about">About</NuxtLink>
 
- 
-
-  <!-- ... -->
+<div class="grid grid-cols-4-[200px_minmax(900px,_1fr)_100px] gap-4 outline outline-offset-2">
+  <div class="outline outline-offset-2">01</div>
+  <div class="outline outline-offset-2">02</div>
+  <div class="outline outline-offset-2">03</div>
+  <div class="outline outline-offset-2">04</div>
+  <div class="outline outline-offset-2">05</div>
+  <div class="outline outline-offset-2">06</div>
+  <div class="outline outline-offset-2">07</div>
+  <div class="outline outline-offset-2">08</div>
+  <div class="outline outline-offset-2">09</div>
+  <div class="outline outline-offset-2">10</div>
+  <div class="outline outline-offset-2">11</div>
+  <div class="outline outline-offset-2">12</div>
 </div>
+
 </template>
 
 
