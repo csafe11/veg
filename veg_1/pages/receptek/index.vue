625d86432a5a1e8d37624e927bcf8b9fa76bd80b
csafe11 <biztonsagos01@gmail.com>
2025-03-17 22:42:33 +0100

    harmadik

--- veg_1/pages/receptek/index.vue
+++ veg_1/pages/receptek/index.vue
@@ -40,15 +40,15 @@
       <!-- Másik kártya -->
       <div class="flex">
         <div class="w-full bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 flex flex-col">
-          <a href="#">
+        
             <NuxtImg class="aspect-3/2 object-cover w-full" src="cover8.jpg" />
-          </a>
+         
           <div class="p-5 flex flex-col flex-grow">
-            <a href="#">
+          
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 Noteworthy technology acquisitions 2021
               </h5>
-            </a>
+         
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2021 so far.
             </p>
